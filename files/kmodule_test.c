996ad2e91739c2db9853701ef5b225653d6777d8
lester <lester_hu@bandrich.com>
2014-08-22 19:30:04 +0800

    very simple kernel module pattern

--- files/kmodule_test.c
+++ files/kmodule_test.c
@@ -4,19 +4,28 @@
  * PUBLIC DOMAIN
  */
 
+
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/version.h>
 
-int init_module(void)
+MODULE_DESCRIPTION("Kernel Module Test");
+MODULE_AUTHOR("Lester Hu <lester_hu@bandrich.com>");
+MODULE_LICENSE("BandRich Proprietary license");
+
+static int kernel_module_init (void)
 {
-	printk("Module init called\n");
-
-	// return sucsess
+	printk(KERN_INFO "----------Kernel Module Test Init---------\n");
 	return 0;
 }
 
 
-void cleanup_module(void)
+static void kernel_module_exit (void)
 {
-	printk("Module cleanup called\n");
+	printk(KERN_INFO "----------Kernel Module Test Exit----------\n");
+	return;
 }
+
+module_init(kernel_module_init);
+module_exit(kernel_module_exit);
