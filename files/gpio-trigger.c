9a639f602a64d672be99209e5b9ae9dd56816783
lester <lester_hu@bandrich.com>
2014-08-29 11:57:30 +0800

    remove unused header

--- files/gpio-trigger.c
+++ files/gpio-trigger.c
@@ -1,16 +1,12 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/interrupt.h>	//
-#include <linux/gpio.h>		//
-#include <linux/sysfs.h>	//
+#include <linux/interrupt.h>
+#include <linux/gpio.h>
+#include <linux/sysfs.h>
 #include <linux/device.h>
-// #include <asm/siginfo.h>
-#include <linux/rcupdate.h>	//
-// #include <linux/uaccess.h>
-// #include <linux/slab.h>
-#include <linux/sched.h>	//
-// #include <linux/spinlock.h>
+#include <linux/rcupdate.h>
+#include <linux/sched.h>
 
 #define DRIVER_AUTHOR			"Lester Hu <lester_hu@bandrich.com>"
 #define DRIVER_DESC			"GPIO Interrupt Test"
